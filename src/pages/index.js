58e1f9eff8bc41150679dc3a692223ef8a361c5c
lilylaf <lilyannlafleur@gmail.com>
2023-02-28 16:17:26 -0700

    file clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello NextJs World!</h1>
+      <h1>This Is the Portfolio website of Lily Brown</h1>
     </div>
   );
 }
